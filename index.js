30d1893d7d8572098bb813b59752636413d3f253
Michael Robinson <mike@pagesofinterest.net>
2013-07-15 00:56:49 +1200

    Updated index

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+var Email = null,
+  config = null;
+
 module.exports = {
-  initialise: require('./lib/initialise'),
-  Email: function() {
-    return require('./lib/models/Email').model();
+  initialise: function(app, mongoose, connection, emailConfig){
+    config = emailConfig;
+    require('./lib/initialise')(app, mongoose, connection, emailConfig.transport);
+    Email = require('./lib/models/Email').model();
+  },
+  send: function(options, callback) {
+    options.from = config.defaults.from;
+    var email = new Email(options);
+    email.send(callback);
   }
 }
